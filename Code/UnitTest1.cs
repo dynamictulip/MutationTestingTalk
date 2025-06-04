dddce9b6dff14ea55bc80522fca17f408a1b669a
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-06-04 17:00:32 +0000

    Add basic project

---
+++ Code/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace MutationTestingTalk;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test_that_passes()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void Test_that_fails()
+    {
+        Assert.True(false);
+    }
+}
