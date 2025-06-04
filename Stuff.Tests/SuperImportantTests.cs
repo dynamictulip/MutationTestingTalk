3f1a4bf7b615c7afba6870d3fff779cf2f8259fc
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2025-06-04 20:23:48 +0000

    Start adding stuff to test

---
+++ Stuff.Tests/SuperImportantTests.cs
@@ -0,0 +1,30 @@
+﻿namespace MutationTestingTalk;
+
+public class SuperImportantTests
+{
+    [Fact]
+    public void Test_Hello()
+    {
+        Assert.Equal("Hello", SuperImportant.Hello);
+    }
+
+    [Fact]
+    public void Test_Add()
+    {
+        var actual = SuperImportant.Add(2, 2);
+
+        Assert.Equal(4, actual);
+    }
+
+    [Fact]
+    public void Test_IsLessThanFortyTwo_is_true_when_less()
+    {
+        Assert.True(SuperImportant.IsLessThanFortyTwo(10));
+    }
+
+    [Fact]
+    public void Test_IsLessThanFortyTwo_is_false_when_more()
+    {
+        Assert.False(SuperImportant.IsLessThanFortyTwo(100000));
+    }
+}
--- Stuff/SuperImportant.cs
+++ Stuff/SuperImportant.cs
@@ -1,4 +1,23 @@
+using System.Text.RegularExpressions;
+
 public class SuperImportant
 {
-    public string Hello => "Hello";
+    public static string Hello => "Hello";
+
+    public static int Add(int a, int b)
+    {
+        return a + a;
+    }
+
+    public static bool IsLessThanFortyTwo(int num)
+    {
+        return num < 42;
+    }
+
+    public static bool IsGmailAddress(string input)
+    {
+        string gmailPattern = @"^[a-zA-Z0-9._%+-]+@gmail\.[a-zA-Z]{2,}$";
+
+        return Regex.IsMatch(input, gmailPattern);
+    }
 }
